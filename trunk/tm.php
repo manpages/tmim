778ed0d929cfb23856119629ed189f3d01f2db96
dva.traktorista <dva.traktorista@36acd05f-7ff5-f980-afe6-fad781d53c45>
2010-11-30 22:10:51 +0000

    Turing Machine implementation

---
+++ trunk/tm.php
@@ -0,0 +1,78 @@
+<?php
+Class TM {
+	function __construct() {}
+	static $states = array('HALT' => array(-1, 0, 'HALT'));
+	static $q0 = 'HALT';
+	static $tape = array('E');
+	static $epsi = 'E';
+	static $T = array('0', '1', '_', //2
+			"<?php\ninclude './tm.php';\n", //3
+			'TM::Q(\'', //4
+			'\',\'', //5
+			'\',',   //6
+			',\'',   //7
+			"\');\n", //8
+			'?>', 'E', '!' //11
+	);
+	static function Q($name, $write_symbol, $move_tape, $next_state) {
+		TM::$states[$name] = array($write_symbol, $move_tape, $next_state);
+	}
+	static function q0 ($name, $silent = false) {
+		TM::$q0 = $name;
+		TM::EXE($silent);
+	}
+	static function EXE($silent = false) {
+		$state = TM::$q0.'_'.TM::$tape[0];
+		$state_name = TM::$q0;
+		$i = 0;
+		while ($state_name != 'HALT') {
+			$read = TM::$tape[$i];
+			if (!$silent) {
+				echo "STATE $state\n";
+				echo "CARET $i\n";
+				echo implode(TM::$tape)."\n";
+			}
+			TM::$tape[$i] = TM::$T[TM::$states[$state][0]];
+			$j = $i + TM::$states[$state][1];
+			if ($j == $i)
+				die ('State '.$state.' halts TM unexpectinly.
+		i = '.$i.'
+		t = '.implode(TM::$tape))."\n";
+
+			if ($j <= -1 || $j >= count(TM::$tape)) {
+				if ($j == -1) {
+					TM::$tape = array_reverse(TM::$tape);
+					TM::$tape[] = TM::$epsi;
+					TM::$tape = array_reverse(TM::$tape);
+					$j = 0;
+				} elseif ($j == count(TM::$tape)) {
+					TM::$tape[] = TM::$epsi;
+				} else {
+					die ('State '.$state.' Too long step.
+		i = '.$i.'
+		t = '.TM::$tape);
+				}
+			}
+			$i = $j;
+			$knum = 0;
+			foreach (TM::$T as $KEY_NUM => $SYMBOL) {
+				if ($SYMBOL == $read) {
+					$knum = $KEY_NUM;
+					break;
+				}
+			}
+			$state = TM::$states[$state][2].'_'.$knum;
+			$state_name = TM::$states[$state][2];
+			echo implode(TM::$tape)."\n";
+			echo "CARET $i\n\n";
+		}
+			TM::$tape{$i} = TM::$states[$state][0];
+			if (!$silent) {
+				echo "STATE $state\n";
+				echo implode(TM::$tape)."\n\n";
+				echo "HALT\n";
+			}
+			return TM::$tape;
+	}
+}
+?>
